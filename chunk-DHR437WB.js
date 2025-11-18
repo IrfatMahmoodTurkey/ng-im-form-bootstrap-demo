00b0c8a485e7018e53ac966669f069bbf911482a
Irfat Mahmood Turkey <46050134+IrfatMahmoodTurkey@users.noreply.github.com>
2025-11-18 22:33:56 +0600

    Project files initialized.

---
+++ chunk-DHR437WB.js
@@ -0,0 +1,1 @@
+import{Bb as n,O as t,Y as i}from"./chunk-N5KWBCK7.js";var r=class o{static \u0275fac=function(e){return new(e||o)};static \u0275mod=i({type:o});static \u0275inj=t({imports:[n]})};export{r as a};
