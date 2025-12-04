98e3b26422d96bbb955972fcf3fb02ef4650a2fb
Irfat Mahmood Turkey <46050134+IrfatMahmoodTurkey@users.noreply.github.com>
2025-12-04 11:44:59 +0600

    demo v1.0.1 to v.1.1.0

---
+++ chunk-HWHORJ6W.js
@@ -0,0 +1,1 @@
+import{Cb as n,O as t,Y as i}from"./chunk-C3ADGVGU.js";var r=class o{static \u0275fac=function(e){return new(e||o)};static \u0275mod=i({type:o});static \u0275inj=t({imports:[n]})};export{r as a};
